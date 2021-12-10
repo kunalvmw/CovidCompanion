4e94af56fa97f6b50c305072b219071ff576aebd
kunalvmw <kunalvwadhwa@gmail.com>
2021-12-10 02:59:20 -0500

    first commit

---
+++ firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+//put Details of firebase 
+};
+
+
+
+// Initialize Firebase
+let app;
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app()
+}
+
+const auth = firebase.auth()
+const store = firebase.firestore()
+
+export { auth, store };
